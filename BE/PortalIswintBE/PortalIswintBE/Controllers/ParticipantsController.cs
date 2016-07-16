dbe862890e7b5be8abddb2ae5b534bcbd4051090
DenisTDR <denis.tdr@gmail.com>
2016-07-16 14:16:01 +0300

    st

--- BE/PortalIswintBE/PortalIswintBE/Controllers/ParticipantsController.cs
+++ BE/PortalIswintBE/PortalIswintBE/Controllers/ParticipantsController.cs
@@ -1,9 +1,22 @@
-﻿using PortalIswintBE.Data.Models.Entities;
+﻿using System.Net;
+using System.Threading.Tasks;
+using System.Web.Http;
+using PortalIswintBE.Data.Db;
+using PortalIswintBE.Data.Models.Entities;
 using PortalIswintBE.Data.Models.ViewModels;
 
 namespace PortalIswintBE.Controllers
 {
     public class ParticipantsController : GenericController<Participant, ParticipantViewModel>
     {
+        [System.Web.Mvc.HttpGet]
+        public async Task<IHttpActionResult> GetArrivals()
+        {
+            using (var db = new Database())
+            {
+                
+            }
+            return null;
+        }
     }
 }
